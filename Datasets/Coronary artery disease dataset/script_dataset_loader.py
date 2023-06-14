c80826f1bf640a4dccb7a3b36c3c7d246d2e6e06
MarcosLDC <89137791+MarcosLDC@users.noreply.github.com>
2023-06-14 10:47:29 +0200

    Add files via upload

--- Datasets/Coronary artery disease dataset/script_dataset_loader.py
+++ Datasets/Coronary artery disease dataset/script_dataset_loader.py
@@ -5,6 +5,12 @@ import os
 
 from sklearn.preprocessing import minmax_scale
 from sklearn.impute import KNNImputer
+from scipy import stats
+from collections import Counter
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 def crea_dataset(lista,stop):
 
@@ -81,16 +87,16 @@ def checker(data):
 
 
 
-with open(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\hungarian.data') as f:
+with open(r'hungarian.data') as f:
     Hungrarian = f.read().splitlines()
 
-with open(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\switzerland.data') as f:
+with open(r'switzerland.data') as f:
     Switzerland = f.read().splitlines()
 
-with open(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\long-beach-va.data') as f:
+with open(r'long-beach-va.data') as f:
     Long_beach_va = f.read().splitlines()
 
-with open(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\cleveland_cut.data') as f:
+with open(r'cleveland_cut.data') as f:
     cleveland = f.read().splitlines()
     #try:
     #    cleveland = f.read().splitlines()
@@ -103,58 +109,137 @@ data_s = crea_dataset( Switzerland, 'name')
 data_l = crea_dataset( Long_beach_va, 'name')
 data_c = crea_dataset( cleveland, 'name')
 
-data = data_h + data_s + data_l + data_c
+data = data_c + data_h + data_s + data_l 
+print(len(data_c + data_h))
 
 print(checker(data_h), checker(data_s), checker(data_l), checker(data_c), checker(data))
-
+print(len(data_h), len(data_s), len(data_l), len(data_c))
 
 #3  (age)  #4 (sex)  #9  (cp)  #10 (trestbps)  #12 (chol)  #16 (fbs)  #19 (restecg)   
 #32 (thalach)  #38 (exang) #40 (oldpeak) #41 (slope) #44 (ca) #51 (thal) 
-informative_attributes = [2, 3, 8, 9, 11, 15, 18, 31, 37, 39, 40, 43, 50] # in python list start in 0 not 1
+informative_attributes = [2, 3, 8, 9, 11, 15, 18, 31, 37, 39, 40] # in python list start in 0 not 1
 
 
 if checker(data) == "correct":
-    df = pd.DataFrame.from_records(data)
-    df.to_csv(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\data.csv', index=False)
+    df_ = pd.DataFrame.from_records(data)
+    df_.to_csv(r'data.csv', index=False)
+    df_h = pd.DataFrame.from_records(data_h)
+    df_h.to_csv(r'data_h.csv', index=False)
+    df_s = pd.DataFrame.from_records(data_s)
+    df_s.to_csv(r'data_s.csv', index=False)
+    df_l = pd.DataFrame.from_records(data_l)
+    df_l.to_csv(r'data_l.csv', index=False)
+    df_c = pd.DataFrame.from_records(data_c)
+    df_c.to_csv(r'data_c.csv', index=False)
+
+
 
 X = data
+df_for_analisis = pd.DataFrame.from_records(X)
+
+dataset_name = ""
+
 list_of_index = np.arange(len(X[0])).tolist() # Empieza en 0   # Array must be a list
+
 Y = [ele.pop(57) for ele in X]
+#print(Counter(Y))
+Y = [ele if ele != 4 else 3 for ele in Y ]
+#print(Counter(Y))
 list_of_index.remove(57)
 
 #df_ = pd.DataFrame.from_records(X) 
 
 # basic statistics
-print("Mean by columns:")
-print(df.mean().tolist()) #string features are avoided
-print("Variance by columns:")
-print(df.var().tolist())
-nans = [np.sum((df[i] == -9.0))/len(df) for i in range(len(df.mean().tolist()))]
+nans = [np.sum((df_for_analisis[i] == -9.0))/len(df_for_analisis) for i in range(len(df_for_analisis.mean().tolist()))]
 print("Percentage of Nan by columns:")
-print(nans)
+print(np.around(nans,2).tolist())
 
-deleted_indexes = [i for i in range(len(nans)) if nans[i] > 0.5]
+deleted_indexes = [i for i in range(len(nans)) if nans[i] > 0.25]
+print("deleted indexes: ", deleted_indexes)
+deleted_indexes.append(19)
+deleted_indexes.append(20)
+deleted_indexes.append(21)
+deleted_indexes.append(54)
+deleted_indexes.append(55)
+deleted_indexes.append(56)
+deleted_indexes.append(1)
+deleted_indexes.append(0)
+deleted_indexes.sort()
+print("deleted indexes: ", deleted_indexes)
 
-
-X = np.delete(X, deleted_indexes, axis=1) # delete rows with more than 0.5 of Nan
+X = np.delete(X, deleted_indexes, axis=1) # delete rows with more than 0.25 of Nan
 X = np.delete(X, -1, axis=1)              # delete name column
+
 
 [list_of_index.remove(ele) for ele in deleted_indexes]
 list_of_index.remove(75)
 
+
 print("list of index: ")
 print(list_of_index)
+print("Informative index: ")
+print(informative_attributes)
+
 
 X = np.array(X, dtype='float').tolist()
 
-
-from sklearn.impute import KNNImputer
-imputer = KNNImputer(missing_values = -9.0, n_neighbors=2)
+imputer = KNNImputer(missing_values = -9.0, n_neighbors=5)
 X = imputer.fit_transform(X)
-from sklearn.preprocessing import minmax_scale
-X = minmax_scale(X)
+
+print(np.mean(X, axis=0))
+
+# basic statistics
+print("Mean by columns (In order. After imputing):")
+print(np.around(df_for_analisis.mean(),2).tolist()) #string features are avoided
+print("Variance by columns (In order. After imputing):")
+print(np.around(df_for_analisis.var()).tolist())
+varianza = df_for_analisis.var().tolist()
+
+min_max_index = []
+binary_index = []
+z_score_index = []
+i = 0
+print(len(list_of_index))
+for indx in list_of_index:
+
+    z = Counter(X[:,i])
+    print
+    
+    if len(list(z.keys())) == 2:
+        binary_index.append(indx)
+        i = i+1
+    else:
+        X[:,i] = stats.zscore(X[:,i])
+        z_score_index.append(indx)
+        i = i + 1
+
+        #if len(list(z.keys())) > 10:
+            #X[:,i] = stats.zscore(X[:,i])
+            #z_score_index.append(indx)
+            #i = i + 1
+
+        #else:
+            #X[:,i] = minmax_scale(X[:,i])
+            #min_max_index.append(indx)
+            #i = i + 1
 
 
-np.save(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\attributes.npy', np.array(X))
-np.save(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\target.npy', np.array(Y))
-np.save(r'C:\Users\Marcos\Desktop\Year1\Heart Disease Data Set\features.npy', np.array(list_of_index))
+print("Binary index: ")
+print(binary_index)
+print("Min Max index: ")
+print(min_max_index)
+print("Z score index: ")
+print(z_score_index)
+
+#X = stats.zscore(X, axis=1)
+#X = minmax_scale(X)
+
+print(X[0])
+np.save(r'attributes2' + dataset_name + '.npy', np.array(X))
+#np.save(r'target_join' + dataset_name + '.npy', np.array(Y))
+np.save(r'index2' + dataset_name + '.npy', np.array(list_of_index))
+np.save(r'inf_index2.npy', np.array(informative_attributes))
+
+
+
+
